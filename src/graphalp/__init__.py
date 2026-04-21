47f00466b424862911df2fdaa046cbce940b3694
shivpatri <shivakshit@gmail.com>
2026-04-21 05:32:46 +0530

    Corrected __init__.py and removed Python Notebooks

--- src/graphalp/__init__.py
+++ src/graphalp/__init__.py
@@ -1,18 +1,11 @@
 # src/graphalp/__init__.py
 
-from .graph_label_prediction import grf_predict, mincut_predict, spectral_predict
-from .graph_active_learning import grf_sampler, s2_sampler, spectral_sampler, random_sampler
-from .parametric_graph_models import grf
+from .label_propagation import HarmonicLabelPropagator
+from .parametric_graph_models import GaussianRandomFieldModel
 
 __version__ = "0.1.0"
 
 __all__ = [
-    "grf_predict",
-    "mincut_predict",
-    "s2_sampler",
-    "grf_sampler",
-    "spectral_sampler",
-    "random_sampler",
-    "spectral_predict",
-    "grf"
+    "HarmonicLabelPropagator",
+    "GaussianRandomFieldModel"
 ]
